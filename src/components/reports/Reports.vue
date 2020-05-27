5052c23c306cd827e03489acc22c658ed021545f
VaseCode <zzmworld@outlook.com>
2020-05-27 16:13:32 +0800

    report

--- src/components/reports/Reports.vue
+++ src/components/reports/Reports.vue
@@ -1,30 +1,69 @@
 <template>
-    <div>
-        <el-breadcrumb separator-class="el-icon-arrow-right">
-  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-  <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-  <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-  <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-</el-breadcrumb>
-    </div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width:600px;height:400px"></div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import _ from 'lodash'
+
 export default {
   data () {
     return {
-
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
     }
   },
-  created () {
+  created () {},
+  async mounted () {
+    const myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取图表数据失败')
+    }
 
+    const result = _.merge(res.data, this.options)
+
+    myChart.setOption(result)
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
